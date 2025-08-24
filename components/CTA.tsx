3efb4370e70f1f875627c8d108095320880e6e46
Nishant-Kr-Shaw <shawnishantkr1@gmail.com>
2025-08-24 12:40:14 +0530

    implemented Auth, Billing, Database Setup, Create Comapanion Server Action

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const CTA = () => {
   return (
     <section className="cta-section">
-      <div className="cta-badge">Start learning your way</div>
+      <div className="cta-badge">Start learning your way!</div>
       <h2 className="text-2xl font-bold"> Build and Personalize Learning Companion </h2>
       <p>Pick a name, subject, voice, & personality - and start learning through voice that feel natural and fun</p>
       <Image src="images/cta.svg" alt="cta" width={362} height={232} />
